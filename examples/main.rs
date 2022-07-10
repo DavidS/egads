f44bdbb2ff4c46f4620b09ae770f69341492e81b
David Schmitt <david@black.co.at>
2022-07-10 20:53:28 +0100

    Optimize example to run in under 4s

--- examples/main.rs
+++ examples/main.rs
@@ -1,16 +1,11 @@
-use clap::Parser;
+use egads::descriptor::from_str;
+use egads::fetcher::build_fetcher;
+use futures::stream;
+use futures::StreamExt;
 use opentelemetry::global::shutdown_tracer_provider;
-use tracing::{info, instrument, warn};
-use tracing_subscriber::fmt;
+use tracing::{debug, info, info_span, Instrument};
 use tracing_subscriber::layer::SubscriberExt;
 use tracing_subscriber::Registry;
-
-#[derive(Parser)]
-struct Cli {
-    /// The path to the file to read
-    #[clap(parse(from_os_str))]
-    path: std::path::PathBuf,
-}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync + 'static>> {
@@ -21,7 +16,9 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync + 'static>
         .install_batch(opentelemetry::runtime::Tokio)?;
 
     let telemetry = tracing_opentelemetry::layer().with_tracer(tracer);
-    let subscriber = Registry::default().with(telemetry).with(fmt::layer());
+    let subscriber = Registry::default().with(telemetry);
+    //.with(fmt::layer());
+    // .with(EnvFilter::from_default_env());
     tracing::subscriber::set_global_default(subscriber).expect("setting tracing default failed");
 
     exercise_egads().await;
@@ -35,67 +32,37 @@ async fn exercise_egads() {
     let span = info_span!(target: "egads-test", "exercise_egads");
     let _enter = span.enter();
 
-    // println!("Hello, world!");
-    // let args = Cli::parse();
-    // println!("{:#?}", args.path);
+    let client = build_fetcher();
 
-    // let file = File::open(&args.path).expect("could not open file");
-    // let reader = BufReader::new(file);
-    // let api: definitions::ApiDescriptor =
-    //     serde_json::from_reader(reader).expect("couldn't parse service description");
-
-    // // println!("{:#?}", api.resources["subscriptions"].methods["list"]);
-    // println!("{:#?}", api.schemas["SubscriptionListResponse"]);
-
-    let list = egads::list::fetch()
+    let list = egads::list::fetch(&client)
         .await
         .expect("Failed to load directory list");
-    // println!("{:#?}", list);
+    debug!(?list);
 
-    let mut checks = Vec::new();
-    for item in list.items {
-        // clone the span for each fetch
-        let span = span.clone();
-        checks.push(tokio::spawn(async move {
-            let _descriptor = egads::descriptor::fetch_item(&item)
-                .instrument(span)
-                .await
-                .expect("error retrieving item descriptor");
-        }));
-    }
-
-    let total = checks.len();
     let mut successes = 0;
     let mut errors = 0;
 
-    for check in checks {
-        match check.await.ok() {
-            Some(_) => successes += 1,
-            None => errors += 1,
-        }
-    }
+    stream::iter(list.items.into_iter())
+        .map(|item| {
+            let client = &client;
+            let url = item.discovery_rest_url;
+            async move {
+                debug!("Starting descriptor fetch");
+                let resp = client.get(url).send().await?;
+                debug!("Fetching descriptor response");
+                let body = resp.text().await?;
+                debug!("Parsing response");
+                from_str(body)
+            }
+            .instrument(info_span!("fetch and process"))
+        })
+        .buffer_unordered(150)
+        .map(|r| match r {
+            Ok(_) => successes += 1,
+            Err(_) => errors += 1,
+        })
+        .collect::<Vec<_>>()
+        .await;
 
-    info!(
-        "{} items processed: {} successes, {} errors",
-        total, successes, errors
-    );
-    // let mut done = true;
-    // let current = "set this to a api name";
-
-    // for item in list.items {
-    //     if current == item.name {
-    //         done = false;
-    //     }
-    //     if done {
-    //         continue;
-    //     }
-    //     println!(
-    //         "Fetching {} ({}) from {}",
-    //         item.name, item.title, item.discovery_rest_url
-    //     );
-    //     let descriptor = egads::descriptor::fetch_item(&client, &item)
-    //         .await
-    //         .expect("error retrieving item descriptor");
-    //     println!("{:#?}", descriptor.description);
-    // }
+    info!("processed: {} successes, {} errors", successes, errors);
 }
